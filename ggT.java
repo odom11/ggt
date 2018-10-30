a4c8022374a093f2bcc09a6a0179efb8749c0759
datAss <foobar@foo.com>
2018-10-30 22:15:56 +0100

    fixed 0 issue

--- ggT.java
+++ ggT.java
@@ -2,11 +2,15 @@ public final class ggT {
     public static void main(String[] args) {
         int first = Integer.parseInt(args[0]);
         int second = Integer.parseInt(args[1]);
+        if (second == 0) {
+            System.out.println("ggt = " + first);
+            return;
+        }
         for (
                 int remainder = first % second; 
                 remainder != 0; 
                 remainder = first % second) {
-            System.out.println("first: " + first + ", second: " + second + ", remainder" + remainder);
+            //System.out.println("first: " + first + ", second: " + second + ", remainder" + remainder);
             first = second;
             second = remainder;
         }
